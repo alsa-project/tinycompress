f85fd7c6073d9927b38185b80c99d122f516ca62
Qais Yousef <qais.yousef@imgtec.com>
2015-05-05 11:21:10 +0100

    src/lib/utils.c: remove this empty file
    utils.c isn't implementing anything so delete it
    
    Signed-off-by: Qais Yousef <qais.yousef@imgtec.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
-LOCAL_SRC_FILES:= src/lib/compress.c src/lib/utils.c
+LOCAL_SRC_FILES:= src/lib/compress.c
 LOCAL_MODULE := libtinycompress
 LOCAL_SHARED_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
