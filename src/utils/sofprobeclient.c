d795c4abb8abd20dbfa78fbd0fd4b27f492a5791
Jyri Sarha <jyri.sarha@linux.intel.com>
2026-03-31 00:47:33 +0300

    utils: sofprobeclient: embed SOF probe parser for real-time demux
    Integrate the sof-probes demux engine directly into sofprobeclient so
    captured compressed data is parsed in real time instead of being
    written to a raw file.
    
    Log output from non-audio probe points is printed to stdout
    (equivalent to sof-probes -l). Audio probe data is extracted into
    buffer_<id>.wav files in the current directory. The file output
    parameter is removed; sofprobeclient no longer acts as a generic
    recorder.
    
    New files copied from SOF v2.9 sources tools/probes/ with include
    paths adjusted for standalone tinycompress build:
      probes_demux.c / probes_demux.h  - probe stream parser
      probes_wave.h                    - WAV header definitions
      probe_dma_frame.h                - DMA frame packet format

--- src/utils/sofprobeclient.c
+++ src/utils/sofprobeclient.c
@@ -77,87 +77,40 @@
 #include "sound/compress_params.h"
 #include "sound/compress_offload.h"
 #include "tinycompress/tinycompress.h"
-#include "tinycompress/tinywave.h"
+
+#include "probes_demux.h"
 
 static int verbose;
-static int file;
 static FILE *finfo;
-static bool streamed;
 
 static const unsigned int DEFAULT_CHANNELS = 4;
 static const unsigned int DEFAULT_RATE = 48000;
 static const unsigned int DEFAULT_FORMAT = SNDRV_PCM_FORMAT_S32_LE;
 static const unsigned int DEFAULT_CODEC_ID = SND_AUDIOCODEC_PCM;
 
-static const struct {
-	const char *name;
-	unsigned int id;
-} codec_ids[] = {
-	{ "PCM", SND_AUDIOCODEC_PCM },
-	{ "MP3", SND_AUDIOCODEC_MP3 },
-	{ "AMR", SND_AUDIOCODEC_AMR },
-	{ "AMRWB", SND_AUDIOCODEC_AMRWB },
-	{ "AMRWBPLUS", SND_AUDIOCODEC_AMRWBPLUS },
-	{ "AAC", SND_AUDIOCODEC_AAC },
-	{ "WMA", SND_AUDIOCODEC_WMA },
-	{ "REAL", SND_AUDIOCODEC_REAL },
-	{ "VORBIS", SND_AUDIOCODEC_VORBIS },
-	{ "FLAC", SND_AUDIOCODEC_FLAC },
-	{ "IEC61937", SND_AUDIOCODEC_IEC61937 },
-	{ "G723_1", SND_AUDIOCODEC_G723_1 },
-	{ "G729", SND_AUDIOCODEC_G729 },
-/* BESPOKE isn't defined on older kernels */
-#ifdef SND_AUDIOCODEC_BESPOKE
-	{ "BESPOKE", SND_AUDIOCODEC_BESPOKE },
-#endif
-};
-#define CREC_NUM_CODEC_IDS (sizeof(codec_ids) / sizeof(codec_ids[0]))
-
-static const char *codec_name_from_id(unsigned int id)
-{
-	static char hexname[12];
-	int i;
-
-	for (i = 0; i < CREC_NUM_CODEC_IDS; ++i) {
-		if (codec_ids[i].id == id)
-			return codec_ids[i].name;
-	}
-
-	snprintf(hexname, sizeof(hexname), "0x%x", id);
-	return hexname; /* a static is safe because we're single-threaded */
-}
+static struct dma_frame_parser *parser;
 
 static void usage(void)
 {
-	int i;
-
-	fprintf(stderr, "usage: sofprobeclient [OPTIONS] [filename.wav]\n"
-		"-c\tcard number\n"
-		"-d\tdevice node\n"
-		"-b\tbuffer size\n"
-		"-f\tfragments\n"
+	fprintf(stderr, "usage: sofprobeclient [OPTIONS]\n"
+		"-c\tcard number (default 3)\n"
+		"-d\tdevice node (default 0)\n"
+		"-b\tbuffer size (default 8192)\n"
+		"-f\tfragments (default 4)\n"
 		"-v\tverbose mode\n"
-		"-l\tlength of record in seconds\n"
+		"-l\tlength of record in seconds (0 = unlimited)\n"
 		"-h\tPrints this help list\n\n"
 		"-C\tSpecify the number of channels (default %u)\n"
 		"-R\tSpecify the sample rate (default %u)\n"
-		"-F\tSpecify the format: S16_LE, S32_LE (default S16_LE)\n"
-		"-I\tSpecify codec ID (default %s)\n\n"
-		"If filename.wav is not given the output is written to stdout\n"
-		"Only PCM data can be written to a WAV file.\n\n"
+		"-F\tSpecify the format: S16_LE, S32_LE (default S32_LE)\n\n"
+		"Captured probe data is parsed in real time.\n"
+		"Log output (non-audio probes) is written to stdout.\n"
+		"Audio probe data is written to buffer_<id>.wav files\n"
+		"in the current directory.\n\n"
 		"Example:\n"
-		"\tsofprobeclient -c 1 -d 2 test.wav\n"
-		"\tsofprobeclient -f 5 test.wav\n"
-		"\tsofprobeclient -I BESPOKE >raw.bin\n\n"
-		"Valid codec IDs:\n",
-		DEFAULT_CHANNELS, DEFAULT_RATE,
-		codec_name_from_id(DEFAULT_CODEC_ID));
-
-	for (i = 0; i < CREC_NUM_CODEC_IDS; ++i)
-		fprintf(stderr, "%s%c", codec_ids[i].name,
-		((i + 1) % 8) ? ' ' : '\n');
-
-	fprintf(stderr, "\nor the value in decimal or hex\n");
+		"\tsofprobeclient\n"
+		"\tsofprobeclient -c 1 -d 2\n",
+		DEFAULT_CHANNELS, DEFAULT_RATE);
 
 	exit(EXIT_FAILURE);
 }
@@ -178,67 +131,20 @@ static int print_time(struct compress *compress)
 	return 0;
 }
 
-static int finish_record(void)
-{
-	struct wave_header header;
-	int ret;
-	size_t nread, written;
-
-	if (!file)
-		return -ENOENT;
-
-	/* can't rewind if streaming to stdout */
-	if (streamed)
-		return 0;
-
-	/* Get amount of data written to file */
-	ret = lseek(file, 0, SEEK_END);
-	if (ret < 0)
-		return -errno;
-
-	written = ret;
-	if (written < sizeof(header))
-		return -ENOENT;
-	written -= sizeof(header);
-
-	/* Sync file header from file */
-	ret = lseek(file, 0, SEEK_SET);
-	if (ret < 0)
-		return -errno;
-
-	nread = read(file, &header, sizeof(header));
-	if (nread != sizeof(header))
-		return -errno;
-
-	/* Update file header */
-	ret = lseek(file, 0, SEEK_SET);
-	if (ret < 0)
-		return -errno;
-
-	size_wave_header(&header, written);
-
-	written = write(file, &header, sizeof(header));
-	if (written != sizeof(header))
-		return -errno;
-
-	return 0;
-}
-
-static void capture_samples(char *name, unsigned int card, unsigned int device,
-			    unsigned long buffer_size, unsigned int frag,
-			    unsigned int length, unsigned int rate,
-			    unsigned int channels, unsigned int format,
-			    unsigned int codec_id)
+static void capture_and_parse(unsigned int card, unsigned int device,
+			      unsigned long buffer_size, unsigned int frag,
+			      unsigned int length, unsigned int rate,
+			      unsigned int channels, unsigned int format)
 {
 	struct compr_config config;
 	struct snd_codec codec;
 	struct compress *compress;
-	struct wave_header header;
 	char *buffer;
-	size_t written;
 	int read, ret;
 	unsigned int size, total_read = 0;
 	unsigned int samplebits;
+	uint8_t *parse_buf;
+	size_t parse_len;
 
 	switch (format) {
 	case SNDRV_PCM_FORMAT_S32_LE:
@@ -254,37 +160,16 @@ static void capture_samples(char *name, unsigned int card, unsigned int device,
 
 	if (verbose)
 		fprintf(finfo, "%s: entry, reading %u bytes\n", __func__, length);
-        if (!name) {
-                file = STDOUT_FILENO;
-        } else {
-	        file = open(name, O_RDWR | O_CREAT,
-			    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-	        if (file == -1) {
-		       fprintf(stderr, "Unable to open file '%s'\n", name);
-		       exit(EXIT_FAILURE);
-	        }
-        }
-
-	/* Write a header, will update with size once record is complete */
-        if (!streamed) {
-	    init_wave_header(&header, channels, rate, samplebits);
-	    written = write(file, &header, sizeof(header));
-	    if (written != sizeof(header)) {
-		fprintf(stderr, "Error writing output file header: %s\n",
-			strerror(errno));
-		goto file_exit;
-	    }
-        }
 
 	memset(&codec, 0, sizeof(codec));
 	memset(&config, 0, sizeof(config));
-	codec.id = codec_id;
+	codec.id = DEFAULT_CODEC_ID;
 	codec.ch_in = channels;
 	codec.ch_out = channels;
 	codec.sample_rate = rate;
 	if (!codec.sample_rate) {
 		fprintf(stderr, "invalid sample rate %d\n", rate);
-		goto file_exit;
+		return;
 	}
 	codec.format = format;
 	if ((buffer_size != 0) && (frag != 0)) {
@@ -298,7 +183,7 @@ static void capture_samples(char *name, unsigned int card, unsigned int device,
 		fprintf(stderr, "Unable to open Compress device %d:%d\n",
 			card, device);
 		fprintf(stderr, "ERR: %s\n", compress_get_error(compress));
-		goto file_exit;
+		return;
 	};
 
 	if (verbose)
@@ -311,15 +196,15 @@ static void capture_samples(char *name, unsigned int card, unsigned int device,
 		goto comp_exit;
 	}
 
-	fprintf(finfo, "Recording file %s On Card %u device %u, with buffer of %u bytes\n",
-	       name, card, device, size);
+	fprintf(finfo, "Capturing probes on Card %u device %u, buffer %u bytes\n",
+		card, device, size);
 	fprintf(finfo, "Codec %u Format %u Channels %u, %u Hz\n",
 	       codec.id, codec.format, codec.ch_out, rate);
 
 	compress_start(compress);
 
 	if (verbose)
-		fprintf(finfo, "%s: Capturing audio NOW!!!\n", __func__);
+		fprintf(finfo, "%s: Capturing probe data NOW!!!\n", __func__);
 
 	do {
 		read = compress_read(compress, buffer, size);
@@ -336,12 +221,20 @@ static void capture_samples(char *name, unsigned int card, unsigned int device,
 		if (read > 0) {
 			total_read += read;
 
-			written = write(file, buffer, read);
-			if (written != (size_t)read) {
-				fprintf(stderr, "Error writing output file: %s\n",
-					strerror(errno));
+			/* Feed captured data into the probe parser */
+			parser_fetch_free_buffer(parser, &parse_buf, &parse_len);
+			if ((size_t)read > parse_len) {
+				fprintf(stderr, "Warning: read %d > parser buffer %zu, truncating\n",
+					read, parse_len);
+				read = parse_len;
+			}
+			memcpy(parse_buf, buffer, read);
+			ret = parser_parse_data(parser, read);
+			if (ret < 0) {
+				fprintf(stderr, "Parser error %d, stopping\n", ret);
 				goto buf_exit;
 			}
+
 			if (verbose) {
 				print_time(compress);
 				fprintf(finfo, "%s: read %d\n", __func__, read);
@@ -355,19 +248,13 @@ static void capture_samples(char *name, unsigned int card, unsigned int device,
 		fprintf(stderr, "ERR: %s\n", compress_get_error(compress));
 	}
 
-	ret = finish_record();
-	if (ret < 0) {
-		fprintf(stderr, "Failed to finish header: %s\n", strerror(ret));
-		goto buf_exit;
-	}
+	/* Finalize any open wave files */
+	finalize_wave_files(parser);
 
 	if (verbose)
 		fprintf(finfo, "%s: exit success\n", __func__);
 
 	free(buffer);
-	close(file);
-	file = 0;
-
 	compress_close(compress);
 
 	return;
@@ -375,8 +262,6 @@ buf_exit:
 	free(buffer);
 comp_exit:
 	compress_close(compress);
-file_exit:
-	close(file);
 
 	if (verbose)
 		fprintf(finfo, "%s: exit failure\n", __func__);
@@ -386,34 +271,38 @@ file_exit:
 
 static void sig_handler(int signum __attribute__ ((unused)))
 {
-	finish_record();
-
-	if (file)
-		close(file);
+	/* Finalize wave files on signal */
+	if (parser)
+		finalize_wave_files(parser);
 
 	_exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
 {
-	char *file;
 	unsigned long buffer_size = 8192;
-	int c, i;
+	int c;
 	unsigned int card = 3, device = 0, frag = 4, length = 0;
 	unsigned int rate = DEFAULT_RATE, channels = DEFAULT_CHANNELS;
 	unsigned int format = DEFAULT_FORMAT;
-	unsigned int codec_id = DEFAULT_CODEC_ID;
 
 	if (signal(SIGINT, sig_handler) == SIG_ERR) {
 		fprintf(stderr, "Error registering signal handler\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (argc < 1)
-		usage();
+	/* Initialize the probe parser with log-to-stdout */
+	parser = parser_init();
+	if (!parser) {
+		fprintf(stderr, "Failed to initialize probe parser\n");
+		exit(EXIT_FAILURE);
+	}
+	parser_log_to_stdout(parser);
 
 	verbose = 0;
-	while ((c = getopt(argc, argv, "hvl:R:C:F:I:b:f:c:d:")) != -1) {
+	finfo = stderr;
+
+	while ((c = getopt(argc, argv, "hvl:R:C:F:b:f:c:d:")) != -1) {
 		switch (c) {
 		case 'h':
 			usage();
@@ -453,47 +342,16 @@ int main(int argc, char **argv)
 				usage();
 			}
 			break;
-		case 'I':
-			if (optarg[0] == '0') {
-				codec_id = strtol(optarg, NULL, 0);
-			} else {
-				for (i = 0; i < CREC_NUM_CODEC_IDS; ++i) {
-					if (strcmp(optarg,
-						   codec_ids[i].name) == 0) {
-						codec_id = codec_ids[i].id;
-						break;
-					}
-				}
-
-				if (i == CREC_NUM_CODEC_IDS) {
-					fprintf(stderr, "Unrecognised ID: %s\n",
-						optarg);
-					usage();
-				}
-			}
-			break;
 		default:
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	if (optind >= argc) {
-		file = NULL;
-		finfo = fopen("/dev/null", "w");
-		streamed = true;
-	} else if (codec_id == SND_AUDIOCODEC_PCM) {
-		file = argv[optind];
-		finfo = stdout;
-		streamed = false;
-	} else {
-		fprintf(stderr, "ERROR: Only PCM can be written to a WAV file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	capture_samples(file, card, device, buffer_size, frag, length,
-			rate, channels, format, codec_id);
+	capture_and_parse(card, device, buffer_size, frag, length,
+			  rate, channels, format);
 
 	fprintf(finfo, "Finish capturing... Close Normally\n");
 
+	parser_free(parser);
 	exit(EXIT_SUCCESS);
 }
